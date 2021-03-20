012c42449563d0cca308f9082cc53a9d3f99393e
SemihTut <yalaantv@gmail.com>
2021-03-20 19:14:48 +0200

    dataprovider & excel

--- src/test/java/OrangeHRM/Tests/LoginPagePositiveTest.java
+++ src/test/java/OrangeHRM/Tests/LoginPagePositiveTest.java
@@ -5,6 +5,7 @@ import OrangeHRM.Pages.MainPage;
 import OrangeHRM.Utilities.BrowserUtils;
 import OrangeHRM.Utilities.ConfigurationReader;
 import OrangeHRM.Utilities.Driver;
+import OrangeHRM.Utilities.ExcelUtil;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -16,24 +17,28 @@ public class LoginPagePositiveTest extends TestBase{
 
     @Test(dataProvider = "LoginData")
     public void test1(String username, String password) throws InterruptedException {
-        new LoginPage()
-                .enterUserName(username)
-                .enterPassword(password)
-                .clickLoginBtn().clickMyProfile();
-        Thread.sleep(1000);
+        try {
+            new LoginPage()
+                    .enterUserName(username)
+                    .enterPassword(password)
+                    .clickLoginBtn().clickMyProfile();
+      /*  Thread.sleep(1000);
         BrowserUtils.clickNewTab(new MainPage().aboutLink);
         Thread.sleep(2000);
         //String chor = Keys.chord(Keys.CONTROL,Keys.PAGE_DOWN);
-        BrowserUtils.goToNewTab(new MainPage().aboutLink);
-        Assert.assertEquals(Driver.get().getTitle(), "OrangeHRM");
+        BrowserUtils.goToNewTab(new MainPage().aboutLink);*/
+            Assert.assertEquals(Driver.get().getTitle(), "OrangeHRM");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
-    @DataProvider(name = "LoginData", parallel = false)
+    @DataProvider(name = "LoginData", parallel = true)
     public Object[][] loginData(){
+        ExcelUtil loginDatas = new ExcelUtil("src/test/resources/dataProvider.xlsx","Sheet1");
+        loginDatas.getColumnsNames().forEach(System.out::println);
+        return loginDatas.getDataArrayWithoutFirstRow();
 
-        return new Object[][]{
-                {"Admin","admin123"},
-        };
     }
 }
